f7d044efbdc61e633dbba521e54c6133e63e29b3
molero3111 <molero3111@gmail.com>
2025-01-29 22:05:57 -0300

    Added envs from shared kit

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,27 +1,8 @@
 import 'dotenv/config';
 import * as joi from 'joi';
+import { validateEnvVars } from 'shared-kit';
 
-interface EnvVars {
-    PORT: number;
-    DATABASE_URL: string;
-}
-
-
-const envVarsSchema = joi.object({
+export const envs = validateEnvVars({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
-}).unknown(true);
-
-const { error, value: validatedEnvVars } = envVarsSchema.validate(process.env);
-
-if (error) {
-    throw new Error(`Config validation error: ${error.message}`);
-}
-
-const envsVars: EnvVars = validatedEnvVars;
-
-export const envs = {
-    PORT: envsVars.PORT,
-    DATABASE_URL: envsVars.DATABASE_URL,
-}
-
+});
